2585b1c5c68de52bb242176a8de755ad7ac6ef88
pieteradejong <pieter.a.dejong@gmail.com>
2015-04-22 14:40:06 -0700

    refactored euclidean and manhattan distance to use Minkowski; added manually calculated tests

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ Similarity: Python implementation of several similarity metrics, including:
 Euclidian, Manhattan, Minkowski, Cosine, Jaccard and others.
 Includes tests.
 
+## Todo: Mahalanobis distance, Kullback Leibler Distance, Mutual Information
+
 LogicGates: Python object oriented implementation of a fundamental building block of computers: logic gates.
 Includes tests.
 
 Fraction: Fraction class with overloaded mathematical methods.
 Includes tests.
+
+
--- similarity.py
+++ similarity.py
@@ -5,21 +5,15 @@ import sys
 class SimilarityMeasures():
 
   def euclidian(self, a, b):
-    return math.sqrt( (a[0] - b[0])**2 + (a[1] - b[1])**2 )
+    return self.minkowski(a, b, 2)
 
   def manhattan(self, a, b):
-    return ( math.fabs(a[0] - b[0]) + math.fabs(a[1] - b[1]) ) 
-
+    return self.minkowski(a, b, 1)
 
   def minkowski(self, a, b, p):
-    if p == 1:
-      return self.manhattan(a, b)
-    elif p == 2:
-      return self.euclidian(a, b)
-    else:
-      sum = (math.fabs(a[0]-b[0]))**p + (math.fabs(a[1]-b[1])**p)
-      return sum**(1/p)
-
+    p = float(p) # ensure 1/p is accurate when p is int
+    sum = math.fabs(a[0]-b[0])**p + math.fabs(a[1]-b[1])**p 
+    return sum**(1/p)
 
   def cosine(self, a, b):
     dotprod = a[0] * b[0] + a[1] * b[1]
@@ -35,30 +29,37 @@ class SimilarityMeasures():
     size_of_union = len( a_set.union(b_set) )
     return float(size_of_intersection) / size_of_union
     
-
   def main(self):
-    print "Testing distance metrics..."
+    print "Testing distance metrics...\n"
 
-    print "Euclidean distance:"
-    print "Expect ", math.sqrt(5)," " , self.euclidian((0,0), (1,2))
-    print "Expect ", math.sqrt(25+11*11)," " , self.euclidian((-2,-5), (3,6))
+    print "\nEuclidean distance:\n"
+    print "Expect ", math.sqrt(5)," actual: " , self.euclidian((0,0), (1,2))
+    print "Expect ", math.sqrt(25+11*11)," actual: " , self.euclidian((-2,-5), (3,6))
 
-    print "Manhattan distance:"
-    print "Expect 16", self.manhattan((1,2), (10, -5))
-    print "Expect 32", self.manhattan((10, 18), (2, -6))
+    print "\nManhattan distance:\n"
+    print "Expect 16.0, actual: ", self.manhattan((1,2), (10, -5))
+    print "Expect 32.0, actual: ", self.manhattan((10, 18), (2, -6))
 
-    print "minkowski distance:"
-    print "Expect ", self.minkowski((1,2), (10, -5), 1.5)
-    print "Expect ", self.minkowski((10, 18), (2, -6), 1.5)
+    print "\nMinkowski distance:\n"
+    print "Expect: ", math.sqrt(25+11*11), " actual: ", self.minkowski( (-2,-5), (3,6), 2 ) #Euclidan
+    print "Expect: ", self.manhattan((10,18), (2,-6)), " actual: ", self.minkowski( (10,18), (2,-6), 1 ) #Euclidan
+    print "Expect ~12.749", self.minkowski((1,2), (10, -5), 1.5) #manual calc
+    print "Expect ~24.175", self.minkowski((10, 15), (2, -6), 1.5) #manual calc
 
-    print "cosine distance:"
-    print "Expect ", self.cosine((1,2), (10, -5))
-    print "Expect ", self.cosine((10, 18), (2, -6))
+    print "\ncosine distance:\n"
+    print "Expect 0.0", self.cosine((1,2), (10, -5)) #manual calc
+    print "Expect ~-0.676", self.cosine((10, 18), (2, -6))
 
-    print "jaccard distance:"
-    print "Expect .333", self.jaccard(['a', 'b', 'c'], ['a'])
-    #print "Expect ", self.cosine((10, 18), (2, -6))
+    print "\njaccard distance:\n"
+    print "Expect ~.333", self.jaccard(['a', 'b', 'c'], ['a'])
+    print "Expect =.5", self.jaccard(['frodo', 'bilbo', 'gandalf'], ['gollum', 'elrond', 'frodo', 'samwise', 'bilbo', 'gandalf'])
+    print "Expect 0.0", self.jaccard(['a', 'b', 'c'], ['d', 'e', 'f'])
+    print "Expect 1.0", self.jaccard(['a', 'b', 'c'], ['a', 'b', 'c'])
 
+    # todo: calculate all distances for similar numbers -> compare
 
 if __name__ == '__main__':
   SimilarityMeasures().main()
+
+
+
