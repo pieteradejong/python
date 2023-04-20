36a48845cb1e217942ea2ac5310476b820cf3df0
Pieter Arthur de Jong <pieter.a.dejong@gmail.com>
2023-04-20 18:30:01 -0400

    extract arg pasrsing into new function for unit testing

--- myargparse.py
+++ myargparse.py
@@ -1,18 +1,19 @@
-import math
 import argparse
-
-parser = argparse.ArgumentParser(description='Calculate area of rectangle.')
-parser.add_argument('-w', '--width', type=int, required=True, help='Width of rectangle.')
-parser.add_argument('-l', '--length', type=int, required=True, help='Length of rectangle.')
-args = parser.parse_args()
+from sys import argv
 
 def area_rectangle(width: int, length: int) -> int:
     return width * length
 
+def parse_args(_args: list):
+    parser = argparse.ArgumentParser(description='Calculate area of rectangle.')
+    parser.add_argument('-w', '--width', type=int, required=True, help='Width of rectangle.')
+    parser.add_argument('-l', '--length', type=int, required=True, help='Length of rectangle.')
+    return parser.parse_args(_args)
+
 def main() -> None:
-    width, length = args.width, args.length
-    area = area_rectangle(width, length)
-    print(f"Reactangle area is {width} * {length} = {area}")
+    args = parse_args(argv[1:])
+    area = area_rectangle(args.width, args.length)
+    print(f"Reactangle area is {args.width} * {args.length} = {area}")
 
 main()
 
