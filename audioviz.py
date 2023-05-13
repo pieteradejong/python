9c92b69730ca6002bf29d1d2e04d0f2ec74657e8
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-05-13 11:16:43 -0400

    initated outline for audio visualization project

---
+++ audioviz.py
@@ -0,0 +1,19 @@
+"""
+Project: 
+this will read in an audio or video file, analyze the audio pattern, 
+and create a visualization similar to those on audiostreaming websites/apps.
+
+Learning objectives:
+- familiarity with Python multimedia modules
+- usage of audio formats and algorithms
+- create a great visualization with pyplot.
+"""
+
+# step 1: read sample audio file
+
+# step 2: extract audio "pattern"
+
+# step 3: transform audio into usable data format
+
+# step 4: use plotly to create interesting and aesthetic visualization.
+
