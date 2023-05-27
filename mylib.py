3e685dccb27192d771c418c9cda3201da5749f4a
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-05-27 12:21:52 -0400

    add body of function to write csv, working on data validity checks

--- mylib.py
+++ mylib.py
@@ -1,5 +1,7 @@
 from functools import wraps
 from time import time
+import csv
+from typing import List, Tuple, Dict
 
 class bcolors:
     HEADER = '\033[95m'
@@ -25,6 +27,34 @@ def time_function(func):
 @time_function
 def test_timer(num):
     return sum(list(range(num)))
+
+
+def write_to_csv(filehandle: str = None, data: List[List] = None, headers: List = None):
+    """Data must be list (length zero or more) of lists."""
+    if not filehandle:
+        raise ValueError('Must provide file handle')
+    if not data and not headers:
+        raise ValueError('Must provide either column headers or at least one data row')
+    
+    
+
+    messages = []
+    with open('1-1000.txt', 'r') as fh:
+        words = fh.readlines()
+        words = [w.rstrip().lower() for w in words]
+        words = [w for w in words if w.isalpha()]
+        for _ in range(number_of_messages):
+            sample_size = randint(MIN_TEXT_WORDS, MAX_TEXT_WORDS)
+            words_sampled: list = sample(words, sample_size)
+            messages.append(' '.join(words_sampled))
+    return messages
+
+def write_to_csv(TARGET_FILE_CSV, data):
+    with open(TARGET_FILE_CSV, 'w', encoding='UTF-8') as f:
+        writer = csv.writer(f)
+        writer.write(CSV_COLUMN_NAMES)
+
+
 
 def main():
     # test_timer(1_000_000)
