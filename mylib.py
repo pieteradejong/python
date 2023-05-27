d7a2bfbb2d705d9d5e1b0deca802ae671e510532
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-05-27 17:55:18 -0400

    compelte write and read function

--- mylib.py
+++ mylib.py
@@ -2,6 +2,7 @@ from functools import wraps
 from time import time
 import csv
 from typing import List, Tuple, Dict
+from inspect import getmembers, isfunction
 
 class bcolors:
     HEADER = '\033[95m'
@@ -29,37 +30,44 @@ def test_timer(num):
     return sum(list(range(num)))
 
 
-def write_to_csv(filehandle: str = None, data: List[List] = None, headers: List = None):
+def write_to_csv(filehandle: str=None, data: list[list]=None, headers: list=None) -> tuple[str, int]:
     """Data must be list (length zero or more) of lists."""
     if not filehandle:
         raise ValueError('Must provide file handle')
     if not data and not headers:
         raise ValueError('Must provide either column headers or at least one data row')
+    if not isinstance(data, list) or any(not isinstance(d, list) for d in data):
+        raise ValueError('Input data must be a list of zero or more lists.')
+
+    TARGET_FILE_NAME = 'output.csv' # TODO add path
+    with open(TARGET_FILE_NAME, 'w', encoding='UTF-8') as fh:
+        writer = csv.writer(fh)
+        writer.writerow(headers)
+        writer.writerows(data)
+
+    return (filehandle, len(data))
+        
+def read_from_csv(filehandle: str=None) -> list:
+    if not filehandle:
+        raise ValueError('Must provide file handle')
     
-    
-
-    messages = []
-    with open('1-1000.txt', 'r') as fh:
-        words = fh.readlines()
-        words = [w.rstrip().lower() for w in words]
-        words = [w for w in words if w.isalpha()]
-        for _ in range(number_of_messages):
-            sample_size = randint(MIN_TEXT_WORDS, MAX_TEXT_WORDS)
-            words_sampled: list = sample(words, sample_size)
-            messages.append(' '.join(words_sampled))
-    return messages
-
-def write_to_csv(TARGET_FILE_CSV, data):
-    with open(TARGET_FILE_CSV, 'w', encoding='UTF-8') as f:
-        writer = csv.writer(f)
-        writer.write(CSV_COLUMN_NAMES)
-
-
+    with open(filehandle, 'r', newline='') as fh:
+        data = []
+        reader = csv.reader(filehandle)
+        for row in reader:
+            data.append(row)
+    return data
 
 def main():
-    # test_timer(1_000_000)
-    pass
+    print('Testing csv write:')
+    headers = ['city', 'state', 'level']
+    data = [ ['sf', 'CA', 45], ['nyc', 'NY', 12], ['miami', 'FL', 83] ]
+    csv = write_to_csv('test.csv', data, headers)
+    print(csv)
 
 if __name__ == "__main__":
     main()
 
+"""
+TODO Context: added csv read/write defs, add tests by calling; then pytests
+"""
